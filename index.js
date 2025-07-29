5b57c858e01120a954bca3e1207742aac1ded35b
root <187004982+ugbhbeh@users.noreply.github.com>
2025-07-29 04:51:00 +0000

    added auth.js

--- index.js
+++ index.js
@@ -6,11 +6,19 @@ const jwt = require("jsonwebtoken");
 const { PrismaClient } = require("@prisma/client");
 const UserRouter = require("./routes/UserRouter.js");
 const ChatRouter = require("./routes/ChatRouter.js");
+const { Socket } = require("dgram");
+const { error } = require("console");
 
 require("dotenv").config();
 
 const app = express();
 const httpServer = http.createServer(app); 
+const io = new Server(httpServer, {
+    cors: {
+        origin: "*",
+        methods:["GET", "POST"]
+    }
+});
 
 const prisma = new PrismaClient();
 const port = 8080;
@@ -21,6 +29,11 @@ app.use(express.json());
 
 app.use("/users", UserRouter); 
 app.use("/chats", ChatRouter);
+
+io.use((socket, next) => {
+    const token = socket.handshake.auth.token;
+    if (!token) return next(new Error("Invalid token"));
+});
 
 httpServer.listen(port, () => {
   console.log(`Server launched on port ${port}`);
