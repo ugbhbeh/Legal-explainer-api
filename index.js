4d301acb6e205043b0236c2664327b4af5d8ed6f
root <187004982+ugbhbeh@users.noreply.github.com>
2026-03-25 04:15:27 +0000

    removed redundunt code

--- index.js
+++ index.js
@@ -2,8 +2,6 @@ const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
 const cors = require("cors");
-const jwt = require("jsonwebtoken");
-const { PrismaClient } = require('@prisma/client');
 const dotenv = require('dotenv')
 dotenv.config();
 
@@ -14,13 +12,6 @@ const DocumentRouter = require("./routes/DocumentRouter.js");
 const app = express();
 const httpServer = http.createServer(app); 
 
-const io = new Server(httpServer, {
-    cors: {
-        origin: "*",
-        methods:["GET", "POST"]
-    }
-});
-
 const port = 8080;
 
 app.use(cors());
@@ -30,20 +21,6 @@ app.use(express.json());
 app.use("/users", UserRouter); 
 app.use("/chats", ChatRouter);
 app.use("/document", DocumentRouter);
-
-
-
-io.use((socket, next) => {
-    const token = socket.handshake.auth.token;
-    if (!token) return next(new Error("Invalid token"));
-    try {
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        socket.user = decoded;
-        next();
-    } catch (err) {
-        return next(new Error("Authentication failed"));
-    }
-});
 
 httpServer.listen(port, () => {
   console.log(`Server launched on port ${port}`);
